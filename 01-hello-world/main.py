e1346abef801f521ec81cef1d418fb5b7374ff01
Joe Karasha <jkarasha@gmail.com>
2023-12-26 19:45:16 -0800

    Add http codes and tags.

--- 01-hello-world/main.py
+++ 01-hello-world/main.py
@@ -1,8 +1,16 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, responses, status
 
 app = FastAPI()
 
-@app.get("/")
-def read_root() -> Union[str, dict]:
-    return {"Hello": "World"}
+@app.get("/hello/{name}", status_code=status.HTTP_200_OK, tags=["greetings"])
+async def greeting(name: str = "World") -> Union[str, dict]:
+    if name.lower() == "bob":
+        return responses.JSONResponse(
+            content={"error": "Bob is not allowed"},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+    else:
+        return responses.JSONResponse(
+            content={"greeting": f"Hello, {name.title()}"},
+            status_code=status.HTTP_200_OK,)
