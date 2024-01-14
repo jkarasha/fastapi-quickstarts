cba80942551749d9f61d993ce3b2ce3b286b6812
Joe Karasha <jkarasha@gmail.com>
2024-01-14 13:14:03 -0800

    Add docker/docker-compose/pytest

---
+++ tdd-demo/src/app/api/notes.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, status
+
+from app.api import crud
+from app.models import NoteDB, NoteSchema
+
+router = APIRouter()
+
+@router.post("/", response_model=NoteDB, status_code=status.HTTP_201_CREATED)
+async def create_note(payload: NoteSchema):
+    note_id = await crud.post(payload)
+    #
+    response_object = {
+        "id": note_id,
+        "title": payload.title,
+        "description": payload.description
+    }
+    return response_object
