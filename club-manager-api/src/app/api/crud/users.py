28af3673212412b186549fdccbdeef93a6b3bed8
Joe Karasha <jkarasha@gmail.com>
2024-02-07 19:46:53 -0800

    Add user read-all, update, delete operations

--- club-manager-api/src/app/api/crud/users.py
+++ club-manager-api/src/app/api/crud/users.py
@@ -4,3 +4,20 @@ from app.api.models.users import UserSchema
 async def post(payload: UserSchema):
     query = user.insert().values(username=payload.username, email=payload.email, password=payload.password)
     return await database.execute(query=query)
+
+async def get(id: int):
+    query = user.select().where(id == user.c.id)
+    return await database.fetch_one(query=query)
+
+async def get_all():
+    query = user.select()
+    return await database.fetch_all(query=query)
+
+async def put(id: int, payload: UserSchema):
+    query = user.update().where(id == user.c.id).values(username=payload.username, email=payload.email, password=payload.password)
+    return await database.execute(query=query)
+
+async def delete(id: int):
+    query = user.delete().where(id == user.c.id)
+    return await database.execute(query=query)
+
--- club-manager-api/src/tests/test_users.py
+++ club-manager-api/src/tests/test_users.py
@@ -13,4 +13,67 @@ def test_create_user(test_app, monkeypatch):
     #
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json() == test_response_payload
+
+def test_read_user(test_app, monkeypatch):
+    test_data = {"id": 1, "username": "testuser", "email": "testuser@example.com", "password": "password"}
+    async def mock_get(id):
+        return test_data
+    monkeypatch.setattr(crud, "get", mock_get)
+    response = test_app.get("/users/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == test_data
+
+def test_read_user_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+    monkeypatch.setattr(crud, "get", mock_get)
+    response = test_app.get("/users/999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()["detail"] == "User with given id not found"  
+
+def test_read_all_users(test_app, monkeypatch):
+    test_data = [
+        {"id": 1, "username": "testuser", "email": "testuser@example.com", "password": "password"},
+        {"id": 2, "username": "testuser2", "email": "testuser2@examplecom", "password": "password2"},
+    ]
+    async def mock_get_all():
+        return test_data
+    monkeypatch.setattr(crud, "get_all", mock_get_all)
+    response = test_app.get("/users/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == test_data
+
+def test_update_user(test_app, monkeypatch):
+    test_update_data = {"id": 1, "username": "testuser", "email": "testuser@example.com", "password": "password"}
+    async def mock_get(id):
+        return True
+    monkeypatch.setattr(crud, "get", mock_get)
+    async def mock_put(id, payload):
+        return 1
+    monkeypatch.setattr(crud, "put", mock_put)
+    response = test_app.put("/users/1", data=json.dumps(test_update_data))
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == test_update_data
+
+def test_update_user_incorrect_id(test_app, monkeypatch):
+    test_update_data = {"id": 1, "username": "testuser", "email": "testuser@example.com", "password": "password"}
+    async def mock_get(id):
+        return None
+    monkeypatch.setattr(crud, "get", mock_get)
+    response = test_app.put("/users/999", data=json.dumps(test_update_data))
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()["detail"] == "User with given id not found"
+
+def test_delete_user(test_app, monkeypatch):
+    test_data = {"id": 1, "username": "testuser", "email": "testuser@example.com", "password": "password"}
+    async def mock_get(id):
+        return test_data
+    monkeypatch.setattr(crud, "get", mock_get)
+    async def mock_delete(id):
+        return id
+    monkeypatch.setattr(crud, "delete", mock_delete)
+    response = test_app.delete("/users/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == test_data
     
+
