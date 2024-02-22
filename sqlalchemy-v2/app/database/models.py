6ce30b9c5906296132f730177595314e1814d6c7
Joe Karasha <jkarasha@gmail.com>
2024-02-21 21:17:43 -0800

    Add database models

---
+++ sqlalchemy-v2/app/database/models.py
@@ -0,0 +1,35 @@
+import uuid
+
+from sqlalchemy import Column, ForeignKey, Table, orm
+from sqlalchemy.dialects.postgresql import UUID
+
+class Base(orm.DeclarativeBase):
+    """ Base declarative class for all models """
+    pk: orm.Mapped[uuid.UUID] = orm.mapped_column(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+class Ingredient(Base):
+    """ Ingredient model """
+    __tablename__ = "ingredient"
+    name: orm.Mapped[str]
+
+potion_ingredient_association = Table(
+    "potion_ingredient",
+    Base.metadata,
+    Column("potion_id", UUID(as_uuid=True), ForeignKey("potion.pk")),
+    Column("ingredient_id", UUID(as_uuid=True), ForeignKey("ingredient.pk")),
+)
+
+class Potion(Base):
+    """ Potion model """
+    __tablename__ = "potion"
+
+    name: orm.Mapped[str]
+    ingredients: orm.Mapped[list[Ingredient]] = orm.relationship(
+        secondary=potion_ingredient_association,
+        backref="potions",
+        lazy="selectin",
+    )
+    
