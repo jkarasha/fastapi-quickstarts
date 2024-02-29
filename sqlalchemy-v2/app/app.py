8604f06be613448392de57d027eed2a9f43dfe40
Joe Karasha <jkarasha@gmail.com>
2024-02-28 21:04:20 -0800

    Add app startup logic

---
+++ sqlalchemy-v2/app/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from app.api.v1.routes import router as v1_router
+from app.api.v2.routes import router as v2_router
+from app.config import settings
+
+app = FastAPI(
+    title="The apothecary API",
+    docs_url="/api/docs",
+    openapi_url="/api/openapi.json",
+)
+
+app.include_router(v1_router, prefix="/api")
+app.include_router(v2_router, prefix="/api")
