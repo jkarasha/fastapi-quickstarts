b37637446e735ad5e48f460b45cbd798373d3bf7
Joe Karasha <jkarasha@gmail.com>
2024-03-04 18:43:44 -0800

    Fix issues with alembic migrations.

--- sqlalchemy-v2/app/api/models.py
+++ sqlalchemy-v2/app/api/models.py
@@ -1,12 +1,10 @@
-import uuid
-
 from pydantic import BaseModel, ConfigDict, Field
 
 class Ingredient(BaseModel):
 
     model_config = ConfigDict(from_attributes=True)
 
-    pk: uuid.UUID
+    pk: int
     name: str
 
 class IngredientPayload(BaseModel):
@@ -18,11 +16,11 @@ class Potion(BaseModel):
     
     model_config = ConfigDict(from_attributes=True)
 
-    pk: uuid.UUID
+    pk: int
     name: str
     ingredients: list[Ingredient]
 
 class PotionPayload(BaseModel):
     
     name: str = Field(min_length=1, max_length=128)
-    ingredients: list[uuid.UUID] = Field(min_length=1)
+    ingredients: list[int] = Field(min_length=1)
