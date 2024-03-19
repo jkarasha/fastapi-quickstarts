a38153ade1d3ef81037ab9cf2f6aebe0debba3f9
Joe Karasha <jkarasha@gmail.com>
2024-03-18 20:35:21 -0700

    Add child routes

--- hopkins-assessment-api/app/api/routes.py
+++ hopkins-assessment-api/app/api/routes.py
@@ -19,7 +19,7 @@ async def create_assessor(
     await session.refresh(assessor)
     return schemas.Assessor.model_validate(assessor)
 
-@router.get("/assessor", status_code=status.HTTP_200_OK)
+@router.get("/assessors", status_code=status.HTTP_200_OK)
 async def get_assessors(
     session: AsyncSession = Depends(get_db_session)
 ) -> list[schemas.Assessor]:
@@ -50,7 +50,7 @@ async def create_role(
     await session.refresh(role)
     return schemas.Role.model_validate(role)
 
-@router.get("/role", status_code=status.HTTP_200_OK)
+@router.get("/roles", status_code=status.HTTP_200_OK)
 async def get_roles(
     session: AsyncSession = Depends(get_db_session)
 ) -> list[schemas.Role]:
@@ -69,3 +69,34 @@ async def get_role(
             detail="Role does not exist"
         )
     return schemas.Role.model_validate(role)
+
+@router.post("/child", status_code=status.HTTP_201_CREATED)
+async def create_child(
+    data: schemas.ChildPayload,
+    session: AsyncSession = Depends(get_db_session)
+) -> schemas.Child:
+    child = db_models.Child(**data.model_dump())
+    session.add(child)
+    await session.commit()
+    await session.refresh(child)
+    return schemas.Child.model_validate(child)
+
+@router.get("/children", status_code=status.HTTP_200_OK)
+async def get_children(
+    session: AsyncSession = Depends(get_db_session)
+) -> list[schemas.Child]:
+    children = await session.scalars(select(db_models.Child))
+    return [schemas.Child.model_validate(child) for child in children]
+
+@router.get("/child/{pk}", status_code=status.HTTP_200_OK)
+async def get_child(
+    pk: int,
+    session: AsyncSession = Depends(get_db_session)
+) -> schemas.Child:
+    child = await session.get(db_models.Child, pk)
+    if child is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Child does not exist"
+        )
+    return schemas.Child.model_validate(child)
